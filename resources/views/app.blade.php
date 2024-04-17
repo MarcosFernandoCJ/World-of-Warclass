1792ed9a1fb5dd359d18fa892524c35b85737ffa
TheShepsMT <theshepsmt@gmail.com>
2024-04-17 18:02:18 -0500

    Proyecto hecho con Laravel

---
+++ resources/views/app.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <title>World of Classcraft</title>
+
+<body>
+    {{-- <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Botón</button> --}}
+    <main class="container flex ">
+        {{-- Add first side bar --}}
+        <aside class="bg-cyan-500 fixed top-0 left-0 h-screen w-16 m-0 felx flex-col text-white shadow-lg">
+            <ul>
+                <li class="relative flex items-center justify-center "><a href="">C</a></li>
+                <li class="relative flex items-center justify-center "><a href="">A</a></li>
+                <li class="relative flex items-center justify-center "><a href="">M</a></li>
+                <li class="relative flex items-center justify-center "><a href="">A</a></li>
+            </ul>
+        </aside>
+
+    </main>
+</body 
+</html>
